07ad483c365c7e4f984349ba148ba2011b1c9f1f
Macostik <y.granchenko@gmail.com>
2020-12-21 15:07:38 +0200

    create app

---
+++ RandomMeal_use_Combine/RandomMeal_use_Combine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RandomMeal_use_Combine
+//
+//  Created by Maxim Granchenko on 21.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomMeal_use_Combine/RandomMeal_use_Combine/RandomMeal_use_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomMeal_use_CombineApp.swift
+//  RandomMeal_use_Combine
+//
+//  Created by Maxim Granchenko on 21.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct RandomMeal_use_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
